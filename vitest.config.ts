4198e51f267bb758aa443c9e2a65ea8e6c370add
Ryan Parker <ryan.parker@digital.cabinet-office.gov.uk>
2025-12-08 14:19:27 +0000

    feat(NOT-4): Initial setup for vitests

---
+++ vitest.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+    test: {
+        globals: true,
+        include:['**/*.{test,test.unit}.ts'],
+
+        coverage: {
+            enabled: true, 
+            provider: 'v8', 
+            thresholds: { 
+                statements: 80, 
+                branches: 80, 
+                functions: 80, 
+                lines: 80
+            },
+            include:['src/**/*.ts'],
+            exclude:['src/**/*.{test,test.unit}.ts', ]
+        }
+    }
+})
