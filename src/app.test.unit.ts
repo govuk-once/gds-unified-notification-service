40d33e359d4bb23698e1c8dd60f7b7d6ea3cd7b7
ryanparker-gds <ryan.parker@digital.cabinet-office.gov.uk>
2025-12-09 10:10:25 +0000

    feat(NOT-4): Initial setup for vitests (#7)
    * feat(NOT-4): Initial setup for vitests
    
    * fix(NOT-4): added mock test and update to coverage

---
+++ src/app.test.unit.ts
@@ -0,0 +1,20 @@
+import { describe, it, expect } from 'vitest';
+import { MockMessageService } from './app.ts';
+
+describe('MockMessageService', ()  => {
+    let mockMessageService: MockMessageService;
+
+    beforeEach(() => {
+        mockMessageService = new MockMessageService;
+    });
+
+    describe('testing vitest', () => {
+        it('hello UK!', () => {
+
+            const result = mockMessageService.processMessage();
+
+            expect(result.context).toBe('Hello UK!');
+            expect(result.status).toBe('SENT');
+        });
+    });
+});
---
+++ src/app.ts
@@ -0,0 +1,16 @@
+export interface MockMessageResponse {
+    userId: number;
+    context: string;
+    status: 'SENT' | 'OPENED' | 'READ' | 'EXPIRED' | 'FAILED';
+}
+
+export class MockMessageService {
+    public processMessage(): MockMessageResponse {
+
+        return {
+            userId: 11,
+            context: "Hello UK!",
+            status: "SENT"
+        };
+    }
+}
