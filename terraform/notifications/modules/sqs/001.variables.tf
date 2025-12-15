6055568ce9dfa4273dc8c0ba0c575019be989927
Toby.Fox <toby.fox@digital.cabinet-office.gov.uk>
2025-12-15 15:47:10 +0000

    feat(NOT-12): Added sqs module Added tags for sqs module Added relevant parameters Added dlq for sqs module

---
+++ terraform/notifications/modules/sqs/001.variables.tf
@@ -0,0 +1,48 @@
+/** Metadata **/
+variable "queue_name" {
+  type        = string
+  description = "Name of the queue."
+}
+
+variable "queue_name" {
+  type        = string
+  description = "Name of the queue."
+}
+
+variable "tags" {
+  description = "Tags to apply to the resource."
+  type        = map(string)
+  default     = {}
+}
+
+variable "delay_seconds" {
+  type = number
+  description = "The time in seconds that the delivery of all messages in the queue will be delayed"
+}
+
+variable "max_message_size" {
+  type = number
+  description = "The limit of how many bytes a message can contain before Amazon SQS rejects it"
+}
+
+variable "message_retention_seconds" {
+  type = number
+  description = "The number of seconds Amazon SQS retains a message."
+}
+
+variable "receive_wait_time_seconds" {
+  type = number
+  description = "The time for which a ReceiveMessage call will wait for a message."
+}
+
+variable "redrive_policy_max_receives" {
+  description = "The number of times a message will be received before being moved to the DLQ."
+  type        = number
+  default     = 4
+}
+
+variable "visibility_timeout_seconds" {
+  description = "The length of time during which a message will be unavailable after a consumer receives it."
+  type        = number
+  default     = 30
+}
---
+++ terraform/notifications/modules/sqs/002.sqs.tf
@@ -0,0 +1,26 @@
+// Deploy SQS Queue
+resource "aws_sqs_queue" "this" {
+  // Metadata
+  name                      = var.queue_name
+  tags                      = var.tags
+
+  // Configure instance
+  delay_seconds             = var.delay_seconds
+  max_message_size          = var.max_message_size
+  message_retention_seconds = var.message_retention_seconds
+  receive_wait_time_seconds = var.receive_wait_time_seconds
+  visibility_timeout_seconds = var.visibility_timeout_seconds
+
+  // Dead Letter Queue
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.dlq.arn
+    maxReceiveCount     = 4
+  })
+}
+
+// Dead Letter Queue
+resource "aws_sqs_queue" "dlq" {
+  name                      = "${var.queue_name}-dlq"
+  message_retention_seconds = var.message_retention_seconds
+  tags                      = var.tags
+}
