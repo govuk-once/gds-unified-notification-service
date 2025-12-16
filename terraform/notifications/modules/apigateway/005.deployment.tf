11853a9137d4dac52bd3d40db3bebe6f745249e4
Damian Pokorski <damian.pokorski@digital.cabinet-office.gov.uk>
2025-12-16 16:31:01 +0000

    fix(NOT-13): Enabling api gateway cachine as per tfsec recommendations

--- terraform/notifications/modules/apigateway/005.deployment.tf
+++ terraform/notifications/modules/apigateway/005.deployment.tf
@@ -45,9 +45,11 @@ resource "aws_api_gateway_method_settings" "method" {
   rest_api_id = aws_api_gateway_rest_api.this.id
   stage_name  = aws_api_gateway_stage.this.stage_name
   method_path = "*/*"
+
   settings {
     logging_level      = "INFO"
     data_trace_enabled = true
     metrics_enabled    = true
+    caching_enabled    = true
   }
 }
