e4827a31ab649eccdc6a1366ccdea599ceb446d4
Toby.Fox <toby.fox@digital.cabinet-office.gov.uk>
2025-12-19 15:01:25 +0000

    feat(NOT-18): Added comments to xray group Fixed policy for x-ray group

--- terraform/notifications/modules/lambda/006.xray.group.tf
+++ terraform/notifications/modules/lambda/006.xray.group.tf
@@ -1,15 +1,15 @@
 // Creates an x-ray group
 resource "aws_xray_group" "lambda" {
+  // Metadata
   group_name        = join("-", [var.prefix, "xray", var.xray_group_name])
   filter_expression = var.xray_filter_expression
+  tags              = var.tags
 
-
+  // Config
   insights_configuration {
     insights_enabled      = var.insights_enabled
     notifications_enabled = var.notifications_enabled
   }
-
-  tags = var.tags
 }
 
 // Xray encryption configuration
@@ -23,19 +23,19 @@ resource "aws_iam_policy" "xray_access" {
   name = join("-", [var.prefix, "iamrp", var.function_name, "xraywrite"])
 
   policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
+    Version = "2012-10-17"
+    Statement = [
       {
-        "Effect" : "Allow",
-        "Action" : [
+        Effect = "Allow"
+        Action = [
           "xray:PutTraceSegments",
           "xray:PutTelemetryRecords",
           "xray:GetSamplingRules",
           "xray:GetSamplingTargets",
           "xray:GetSamplingStatisticSummaries"
-        ],
-        "Resource" : [aws_xray_group.lambda.arn]
-      }
+        ]
+        Resource = [aws_cloudwatch_log_group.lambda.arn]
+      },
     ]
   })
 }
