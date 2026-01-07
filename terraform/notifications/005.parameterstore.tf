fa0880456537fa2be0abf960880c1b8daad0498c
Toby.Fox <toby.fox@digital.cabinet-office.gov.uk>
2026-01-07 12:11:34 +0000

    fix(NOT-20): Changing parameter keys to correct syntax

--- terraform/notifications/005.parameterstore.tf
+++ terraform/notifications/005.parameterstore.tf
@@ -5,11 +5,11 @@ module "parameter_store" {
   kms_key_arn = aws_kms_key.main.arn
 
   parameters = {
-    "/config/common/enabled" = "true"
-    "/config/ingest/enabled" = "true"
-    "/config/process/enabled" = "true"
-    "/config/dispatch/enabled" = "true"
+    "config/common/enabled" = "true"
+    "config/ingest/enabled" = "true"
+    "config/process/enabled" = "true"
+    "config/dispatch/enabled" = "true"
     #checkov:skip=CKV_SECRET_6: "Base64 High Entropy String"
-    "/config/dispatch/onetrust/apikey" = "placeholder"
+    "config/dispatch/onetrust/apikey" = "placeholder"
   }
 }
