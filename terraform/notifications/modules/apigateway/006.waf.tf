a191f7ca61154fd74345c85fda508a7d9d3a112c
Toby.Fox <toby.fox@digital.cabinet-office.gov.uk>
2026-01-12 10:08:35 +0000

    feat(NOT-51): Added cloudwatch log group for waf for api gateway Changed naming of rules and metrics for waf

--- terraform/notifications/modules/apigateway/006.waf.tf
+++ terraform/notifications/modules/apigateway/006.waf.tf
@@ -1,4 +1,4 @@
-resource "aws_wafv2_web_acl" "this" {
+resource "aws_wafv2_web_acl" "waf_for_apig" {
   name        = join("-", [var.prefix, "waf", var.name])
   scope       = "REGIONAL"
 
@@ -9,7 +9,7 @@ resource "aws_wafv2_web_acl" "this" {
   # Common Rule Set
   # https://docs.aws.amazon.com/waf/latest/developerguide/aws-managed-rule-groups-baseline.html
   rule {
-    name     = "AWS-AWSManagedRulesCommonRuleSet"
+    name     = "${var.prefix}-common-rule-set"
     priority = 1
     override_action {
       none {}
@@ -22,7 +22,7 @@ resource "aws_wafv2_web_acl" "this" {
     }
     visibility_config {
       cloudwatch_metrics_enabled = true
-      metric_name                = "CommonRuleSet"
+      metric_name                = "${var.prefix}-common-rule-metric"
       sampled_requests_enabled   = true
     }
   }
@@ -31,7 +31,12 @@ resource "aws_wafv2_web_acl" "this" {
 
   visibility_config {
     cloudwatch_metrics_enabled = true
-    metric_name                = "main-metric-name"
+    metric_name                = "${var.prefix}-main-metric"
     sampled_requests_enabled   = true
   }
+}
+
+resource "aws_wafv2_web_acl_logging_configuration" "waf_logging_config" {
+  log_destination_configs = [aws_cloudwatch_log_group.waf_log_group.arn]
+  resource_arn            = aws_wafv2_web_acl.waf_for_apig.arn
 }
