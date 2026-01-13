ccf99be5a223ea9e03de6bf7a6605cd56f614e94
Damian Pokorski <damian.pokorski@digital.cabinet-office.gov.uk>
2026-01-13 08:15:18 +0000

    feat(NOT-81): GH - Adjusting triggers and adding extra logging

--- terraform/notifications/005.parameterstore.tf
+++ terraform/notifications/005.parameterstore.tf
@@ -5,9 +5,9 @@ module "parameter_store" {
   kms_key_arn = aws_kms_key.main.arn
 
   parameters = {
-    "config/common/enabled" = "true"
-    "config/ingest/enabled" = "true"
-    "config/process/enabled" = "true"
+    "config/common/enabled"   = "true"
+    "config/ingest/enabled"   = "true"
+    "config/process/enabled"  = "true"
     "config/dispatch/enabled" = "true"
     #checkov:skip=CKV_SECRET_6: "Base64 High Entropy String"
     "config/dispatch/onetrust/apikey" = "placeholder"
