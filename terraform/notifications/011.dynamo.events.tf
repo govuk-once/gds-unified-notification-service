fb00b42c69672e1740e83a552ca395ce21f81520
Damian Pokorski <damian.pokorski@digital.cabinet-office.gov.uk>
2026-01-30 09:30:42 +0000

    feat(NOT-94): Improved dynamodb attribute and index definition (#65)

--- terraform/notifications/011.dynamo.events.tf
+++ terraform/notifications/011.dynamo.events.tf
@@ -1,10 +1,50 @@
 module "dynamodb_events" {
+  // Metadata
   source     = "./modules/dynamo"
   prefix     = local.prefix
   table_name = "events"
-  hash_key   = "EventID"
-  // TODO: Works with null, does not work with an explicit range index
-  range_key   = null
+
+  // Encrpytion at rest
   kms_key_arn = aws_kms_key.main.arn
   tags        = local.defaultTags
+
+  // Fields
+  hash_key  = "EventID"
+  range_key = "EventDateTime"
+  attributes = [
+    {
+      name = "EventID"
+      type = "S"
+    },
+    {
+      name = "EventDateTime"
+      type = "S"
+    },
+    {
+      name = "NotificationID"
+      type = "S"
+    },
+    {
+      name = "DepartmentID"
+      type = "S"
+    },
+  ]
+
+  // Indexes
+  global_secondary_indexes = [
+    {
+      name               = "NotificationIDIndex"
+      hash_key           = "EventID"
+      range_key          = "NotificationID"
+      projection_type    = "INCLUDE"
+      non_key_attributes = ["EventTimesamp"]
+    },
+    {
+      name               = "DepartmentIDIndex"
+      hash_key           = "EventID"
+      range_key          = "DepartmentID"
+      projection_type    = "INCLUDE"
+      non_key_attributes = ["EventTimesamp"]
+    },
+  ]
 }
