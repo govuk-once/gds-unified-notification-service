7ee0661ebb19f08b09de72626b78eeaf47ae0602
Ryan Parker <ryan.parker@digital.cabinet-office.gov.uk>
2026-03-06 17:05:06 +0000

    fix(NOT-128): schema fix to handle null values

--- src/lambdas/interfaces/IFlexNotification.ts
+++ src/lambdas/interfaces/IFlexNotification.ts
@@ -3,12 +3,12 @@ import z from 'zod';
 export const IFlexNotificationSchema = z
   .object({
     NotificationID: z.string(),
-    Status: z.string(),
+    Status: z.string().optional(),
     NotificationTitle: z.string(),
     NotificationBody: z.string(),
     MessageTitle: z.string().optional(),
     MessageBody: z.string().optional(),
-    DispatchedAt: z.string(),
+    DispatchedAt: z.string().optional(),
   })
   .transform((record) => ({
     ...record,
