5e223345881483998bc2a9345d3b12c96148c19b
Toby.Fox <toby.fox@digital.cabinet-office.gov.uk>
2026-03-26 15:05:22 +0000

    feat(NOT-99): Removed extra #

--- terraform/notifications/modules/apigateway/003.cloudwatch.tf
+++ terraform/notifications/modules/apigateway/003.cloudwatch.tf
@@ -14,7 +14,7 @@ resource "aws_cloudwatch_log_group" "waf_log_group" {
   kms_key_id        = var.kms_key_arn
 }
 
-# # Allow WAF to write logs to cloudwatch
+# Allow WAF to write logs to cloudwatch
 resource "aws_cloudwatch_log_resource_policy" "waf_cloudwatch_logging" {
   policy_document = data.aws_iam_policy_document.waf_logging.json
   policy_name     = join("-", [var.prefix, "iamrp", var.name, "waf-logwrite"])
