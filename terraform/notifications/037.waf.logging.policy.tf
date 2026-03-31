fe714d0bb3212cba7a066dde69b87b8c62a33cf6
Toby.Fox <toby.fox@digital.cabinet-office.gov.uk>
2026-03-31 10:42:33 +0100

    refactor(NOT-187): Move waf logging policy document outside of apigw module to reduce duplication

---
+++ terraform/notifications/037.waf.logging.policy.tf
@@ -0,0 +1,32 @@
+# Allow WAF for each apigw resource to write logs to cloudwatch
+data "aws_iam_policy_document" "waf_logging" {
+  version = "2012-10-17"
+  statement {
+    effect = "Allow"
+    principals {
+      identifiers = ["delivery.logs.amazonaws.com"]
+      type        = "Service"
+    }
+    actions = ["logs:CreateLogStream", "logs:PutLogEvents"]
+    resources = [
+      "${module.api_gateway_pso.aws_cloudwatch_log_group_arn}:*",
+      "${module.api_gateway_flex.aws_cloudwatch_log_group_arn}:*",
+      "${module.api_gateway_flex_private.aws_cloudwatch_log_group_arn}:*"
+    ]
+    condition {
+      test     = "ArnLike"
+      values   = ["arn:aws:logs:${var.region}:${data.aws_caller_identity.aws.account_id}:*"]
+      variable = "aws:SourceArn"
+    }
+    condition {
+      test     = "StringEquals"
+      values   = [tostring(data.aws_caller_identity.aws.account_id)]
+      variable = "aws:SourceAccount"
+    }
+  }
+}
+
+resource "aws_cloudwatch_log_resource_policy" "waf_cloudwatch_logging" {
+  policy_document = data.aws_iam_policy_document.waf_logging.json
+  policy_name     = join("-", [local.prefix, "iamrp", "waf-logwrite"])
+}
