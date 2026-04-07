d9410b87ce5f3e13196e2f0beb8e8982824cd07f
Toby.Fox <toby.fox@digital.cabinet-office.gov.uk>
2026-04-07 14:22:29 +0100

    refactor(NOT-111): Added log group for VPC

---
+++ terraform/notifications/009.vpc.flowlog.tf
@@ -0,0 +1,53 @@
+resource "aws_cloudwatch_log_group" "vpc_cloudwatch_group" {
+  name = join("-", [local.prefix, "vpc", "cloudwatch-group"])
+  tags = local.defaultTags
+}
+
+data "aws_iam_policy_document" "vpc_cloudwatch_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["vpc-flow-logs.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "vpc_cloudwatch_logging_role" {
+  name               = join("-", [local.prefix, "iamr", "vpc", "cloudwatch-role"])
+  assume_role_policy = data.aws_iam_policy_document.vpc_cloudwatch_assume_role.json
+  tags               = local.defaultTags
+}
+
+data "aws_iam_policy_document" "vpc_cloudwatch_logging_policy_document" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "logs:DescribeLogGroups",
+      "logs:DescribeLogStreams",
+    ]
+
+    resources = ["${aws_cloudwatch_log_group.vpc_cloudwatch_group.arn}:*"]
+  }
+}
+
+resource "aws_iam_role_policy" "vpc_cloudwatch_logging_policy" {
+  name   = join("-", [local.prefix, "iamp", "vpc", "cloudwatch-policy"])
+  role   = aws_iam_role.vpc_cloudwatch_logging_role.id
+  policy = data.aws_iam_policy_document.vpc_cloudwatch_logging_policy_document.json
+}
+
+resource "aws_flow_log" "vpc_flow_log" {
+  iam_role_arn         = aws_iam_role.vpc_cloudwatch_logging_role.arn
+  log_destination      = aws_cloudwatch_log_group.vpc_cloudwatch_group.arn
+  log_destination_type = "cloud-watch-logs"
+  traffic_type         = "ALL"
+  vpc_id               = aws_vpc.main.id
+  tags                 = local.defaultTags
+}
