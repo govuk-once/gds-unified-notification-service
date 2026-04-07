d56519d87774891bf6f37ffb682fc1c278007add
Toby.Fox <toby.fox@digital.cabinet-office.gov.uk>
2026-04-07 16:25:50 +0100

    refactor(NOT-111): Made vpc logging only for main environments and addd relevant comments

--- terraform/notifications/010.vpc.flowlog.tf
+++ terraform/notifications/010.vpc.flowlog.tf
@@ -1,13 +1,17 @@
+# Creates a log group to capture the IP traffic
 resource "aws_cloudwatch_log_group" "vpc_cloudwatch_group" {
   #checkov:skip=CKV_AWS_338: "Ensure CloudWatch log groups retains logs for at least 1 year" - duration of retentil to be decided
-  name = "/aws/vpc/${local.prefix}"
+  count = var.is_main_environment_in_account ? 1 : 0
 
+  name              = "/aws/vpc/${local.prefix}"
   retention_in_days = 30
   kms_key_id        = aws_kms_key.main.arn
   tags              = local.defaultTags
 }
 
 data "aws_iam_policy_document" "vpc_cloudwatch_assume_role" {
+  count = var.is_main_environment_in_account ? 1 : 0
+
   statement {
     effect = "Allow"
 
@@ -21,12 +25,17 @@ data "aws_iam_policy_document" "vpc_cloudwatch_assume_role" {
 }
 
 resource "aws_iam_role" "vpc_cloudwatch_logging_role" {
+  count = var.is_main_environment_in_account ? 1 : 0
+
   name               = join("-", [local.prefix, "iamr", "vpc", "cloudwatch-role"])
-  assume_role_policy = data.aws_iam_policy_document.vpc_cloudwatch_assume_role.json
+  assume_role_policy = data.aws_iam_policy_document.vpc_cloudwatch_assume_role[0].json
   tags               = local.defaultTags
 }
 
+# Gives iam permission to add and update logs
 data "aws_iam_policy_document" "vpc_cloudwatch_logging_policy_document" {
+  count = var.is_main_environment_in_account ? 1 : 0
+
   statement {
     effect = "Allow"
 
@@ -37,19 +46,24 @@ data "aws_iam_policy_document" "vpc_cloudwatch_logging_policy_document" {
       "logs:DescribeLogStreams",
     ]
 
-    resources = [aws_cloudwatch_log_group.vpc_cloudwatch_group.arn]
+    resources = [aws_cloudwatch_log_group.vpc_cloudwatch_group[0].arn]
   }
 }
 
 resource "aws_iam_role_policy" "vpc_cloudwatch_logging_policy" {
+  count = var.is_main_environment_in_account ? 1 : 0
+
   name   = join("-", [local.prefix, "iamp", "vpc", "cloudwatch-policy"])
-  role   = aws_iam_role.vpc_cloudwatch_logging_role.id
-  policy = data.aws_iam_policy_document.vpc_cloudwatch_logging_policy_document.json
+  role   = aws_iam_role.vpc_cloudwatch_logging_role[0].id
+  policy = data.aws_iam_policy_document.vpc_cloudwatch_logging_policy_document[0].json
 }
 
+# Setups a flow logs to log the vpc to cloudwatch
 resource "aws_flow_log" "vpc_flow_log" {
-  iam_role_arn         = aws_iam_role.vpc_cloudwatch_logging_role.arn
-  log_destination      = aws_cloudwatch_log_group.vpc_cloudwatch_group.arn
+  count = var.is_main_environment_in_account ? 1 : 0
+
+  iam_role_arn         = aws_iam_role.vpc_cloudwatch_logging_role[0].arn
+  log_destination      = aws_cloudwatch_log_group.vpc_cloudwatch_group[0].arn
   log_destination_type = "cloud-watch-logs"
   traffic_type         = "ALL"
   vpc_id               = aws_vpc.main.id
