fb7042c94d0be7b89ae0b0e6e80f325c189435bd
Toby Fox <toby.fox@digital.cabinet-office.gov.uk>
2026-07-22 14:25:47 +0100

    feat(NOT-356): Consumer secret sharing for events aggregator (#245)
    * fix(NOT-356): Example of consumer sharing for mtls certs
    
    * feat(NOT-356): Added ssm for client certificates
    
    * feat(NOT-356): Added cdk diff to test on pipeline
    
    * feat(NOT-356): Reverting release back to deploy
    
    * feat(NOT-356): Corrected parameter path for certificate consumers
    
    * feat(NOT-356): Added console log to confirm consumer account exists
    
    * feat(NOT-356): Removed extra console log
    
    ---------
    
    Co-authored-by: Damian Pokorski <damian.pokorski@digital.cabinet-office.gov.uk>

--- infrastructure/cdk/config.ts
+++ infrastructure/cdk/config.ts
@@ -124,6 +124,8 @@ export const config = {
       channelId: (await fromSSMJSON<string | null>(`/${namespace}/alerts/slack/channelId`, null))!,
       workspaceId: (await fromSSMJSON<string | null>(`/${namespace}/alerts/slack/workspaceId`, null))!,
     },
+
+    certificateConsumers: await fromSSMJSON<Record<string, string>>(`/${namespace}/certificate/consumers`, {}),
   },
 
   // VPC
