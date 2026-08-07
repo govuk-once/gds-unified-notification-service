3e7de484e0f35c5c6d0f563ae6a14dbfabbfd0a5
Toby.Fox <toby.fox@digital.cabinet-office.gov.uk>
2026-08-07 13:18:43 +0100

    feat(NOT-435): Updated fixture for end2end tests

--- test/e2e/pso.postGroupMessage.test.e2e.ts
+++ test/e2e/pso.postGroupMessage.test.e2e.ts
@@ -1,4 +1,4 @@
-import { test } from '@test/e2e/utils/setup.e2e.vitest';
+import { test, testFixtures } from '@test/e2e/utils/setup.e2e.vitest';
 import { expect } from 'vitest';
 
 const url = () => `/v1/send-to-group`;
@@ -13,6 +13,31 @@ const mockGroupMessage = {
   NotificationTitle: 'You have a new Notification',
   NotificationBody: 'Here is the Notification body.',
 };
+
+beforeAll(async () => {
+  // Setup a user in some group to be able to send a message to that group
+  const flexApi = testFixtures().flexAPI;
+
+  const pushIDUsers = [
+    `a53f62d9-a121-4a16-bd98-da89cd0cdfa0`,
+    `b53f62d9-a121-4a16-bd98-da89cd0cdfa0`,
+    `c53f62d9-a121-4a16-bd98-da89cd0cdfa0`,
+    `d53f62d9-a121-4a16-bd98-da89cd0cdfa0`,
+    `e53f62d9-a121-4a16-bd98-da89cd0cdfa0`,
+  ];
+  const group = [
+    {
+      Namespace: 'test',
+      Group: 'end2end',
+      Subgroup: 'immediate',
+      Action: 'JOIN',
+    },
+  ];
+
+  for (const pushID of pushIDUsers) {
+    await flexApi.post({ path: `/v1/groups?pushID=${pushID}`, body: group });
+  }
+});
 
 describe('POST {{pso}}/send-to-group - Send a group message', () => {
   describe(`Unhappy paths`, () => {
@@ -62,6 +87,55 @@ describe('POST {{pso}}/send-to-group - Send a group message', () => {
           body: {},
         })
       ).rejects.toThrow(`API [POST] ${path} Failed with 400`);
+    });
+  });
+
+  describe(`Happy paths`, () => {
+    test('status 202 and number of users in group when - sending a group message', async ({ psoAPI: api }) => {
+      // Arrange
+      const path = url();
+
+      // Act
+      const result = await api.post({
+        path,
+        body: [mockGroupMessage],
+      });
+
+      // Assert
+      expect(result.status).toEqual(202);
+      expect(result.body).toEqual([
+        {
+          GroupNotificationID: mockGroupMessage.GroupNotificationID,
+          UsersInGroup: 5,
+        },
+      ]);
+    });
+
+    test('status 202 and number of users in group when - sending a group message with no users', async ({
+      psoAPI: api,
+    }) => {
+      // Arrange
+      const path = url();
+
+      // Act
+      const result = await api.post({
+        path,
+        body: [
+          {
+            ...mockGroupMessage,
+            Group: 'nonexistent-group',
+          },
+        ],
+      });
+
+      // Assert
+      expect(result.status).toEqual(202);
+      expect(result.body).toEqual([
+        {
+          GroupNotificationID: mockGroupMessage.GroupNotificationID,
+          UsersInGroup: 0,
+        },
+      ]);
     });
   });
 });
