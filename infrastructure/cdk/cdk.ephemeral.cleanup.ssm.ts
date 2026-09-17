4078526edb20cd81528114fad7b3c6e252fd517f
Toby Fox <toby.fox@dsit.gov.uk>
2026-09-17 17:24:32 +0100

    feat(NOT-268: Fixed ephemeral clean up script

--- infrastructure/cdk/cdk.ephemeral.cleanup.ssm.ts
+++ infrastructure/cdk/cdk.ephemeral.cleanup.ssm.ts
@@ -1,35 +1,45 @@
 import { DeleteParametersCommand, GetParametersByPathCommand, SSMClient } from '@aws-sdk/client-ssm';
 import { config } from 'infrastructure/cdk/config';
 
+const deleteParametersInNamespace = async (ssmClient: SSMClient, nextToken?: string) => {
+  const params = await ssmClient.send(
+    new GetParametersByPathCommand({
+      Path: `/${config.namespace}/`,
+      Recursive: true,
+      WithDecryption: true,
+      MaxResults: 10,
+      NextToken: nextToken,
+    })
+  );
+
+  if (!params.Parameters) {
+    throw new Error('No parameters were found associated with the ephemeral state');
+  }
+
+  const paramsToDelete = params.Parameters.map((p) => p.Name).filter((p) => p !== undefined);
+  await ssmClient.send(
+    new DeleteParametersCommand({
+      Names: paramsToDelete,
+    })
+  );
+
+  if (params.NextToken) {
+    await deleteParametersInNamespace(ssmClient, params.NextToken);
+  }
+};
+
 await (async () => {
   //// =====================================================
   // Ephemeral SSM Cleanup
   //// =====================================================
-  const ssmClient = new SSMClient({ region: config.region });
 
   if (config.isEphemeral) {
     console.log(`Deleting ephemeral parameters.`);
-    const paramsToDelete = await ssmClient.send(
-      new GetParametersByPathCommand({
-        Path: `/${config.namespace}/`,
-        Recursive: true,
-        WithDecryption: true,
-      })
-    );
 
-    if (paramsToDelete.Parameters && paramsToDelete.Parameters.length > 0) {
-      console.log(`Deleting deprecated parameters from namespace.`);
-      const params = paramsToDelete.Parameters.map((p) => p.Name).filter((p) => p !== undefined);
+    const ssmClient = new SSMClient({ region: config.region });
 
-      if (params.length === 0) {
-        throw new Error('No parameters were found associated with the ephemeral state');
-      }
-
-      await ssmClient.send(
-        new DeleteParametersCommand({
-          Names: params,
-        })
-      );
-    }
+    await deleteParametersInNamespace(ssmClient);
+  } else {
+    console.log(`This environment is not ephemeral.`);
   }
 })();
